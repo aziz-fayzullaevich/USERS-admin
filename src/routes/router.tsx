5e297c3d8023eb8e054212892e314713ba4811f2
Aziz Atagullaev <azizatagullaev1@gmail.com>
2026-09-17 18:58:49 +0500

    added home page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,12 +3,17 @@ import { ROUTES } from "./routes";
 import App from "../app/app";
 import UsersPage from "../pages/users";
 import CreateUserPage from "../pages/users/create";
+import HomePage from "../pages/home-page/home-page";
 
 export const router = createBrowserRouter([
   {
     path: ROUTES.HOME,
     element: <App />,
     children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
       {
         path: ROUTES.USERS,
         element: <UsersPage />,
